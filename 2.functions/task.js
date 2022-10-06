e38106279891aaee2860b7897038e485520aaebb
arinaaitova <pagezoso69@gmail.com>
2022-10-06 17:03:27 +0300

    Add files via upload

--- 2.functions/task.js
+++ 2.functions/task.js
@@ -1,31 +1,68 @@
 // Задание 1
 function getArrayParams(arr) {
-  let min, max, sum, avg;
-
-  // Ваш код
-
-  return { min: min, max: max, avg: avg };
-}
-
-// Задание 2
-function worker(arr) {
-  let sum;
-
-  // Ваш код
-
-  return sum;
-}
-
-function makeWork(arrOfArr, func) {
-  let max;
-
-  // Ваш кода
-  // for ...
+    let min = Infinity;
+    let max = -Infinity;
+    let sum = 0;
+    let avg = 0;
   
-  return max;
-}
-
-// Задание 3
-function worker2(arr) {
-  // Ваш код
-}
+    for (let i = 0; i < arr.length; i++){
+      sum += arr[i];
+  
+      if (arr[i] > max) {
+        max = arr[i];
+      }
+  
+      if (arr[i] < min) {
+        min = arr[i];
+      }
+    }
+  
+    avg = parseFloat((sum / arr.length).toFixed(2));
+    return { min: min, max: max, avg: avg };
+  }
+  
+  // Задание 2
+  function worker(arr) {
+    let sum;
+    sum = 0;
+    
+    for (let i = 0; i < arr.length; i++) {
+      sum = sum + arr[i];
+    }
+  
+    return sum;
+  }
+  
+  function makeWork(arrOfArr, func) {
+    let max;
+    max = -Infinity;
+  
+    for (let j = 0; j < arrOfArr.length; j++) {
+  
+      if (func(arrOfArr[j]) > max) {
+        max = func(arrOfArr[j]);
+      }
+  
+    }
+    
+    return max;
+  }
+  
+  // Задание 3
+  function worker2(arr) {
+    let max = -Infinity;
+    let min = Infinity;
+  
+    for (let i = 0; i < arr.length; i++) {
+  
+      if (arr[i] > max) {
+        max = arr[i];
+      }
+  
+      if(arr[i] < min) {
+        min = arr[i];
+      }
+    }
+  
+    return Math.abs(max - min);
+  }
