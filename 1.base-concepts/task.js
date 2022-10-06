d421df5562e6943e14f66fbb924ec0ae05696496
arinaaitova <pagezoso69@gmail.com>
2022-10-06 17:10:04 +0300

    Add files via upload

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,13 +1,18 @@
+"use strict";
+
 function solveEquation(a, b, c) {
-  let arr;
-  // код для задачи №1 писать здесь
-  return arr; // array
-}
+    let arr = [];
+    let d = b**2  - 4 * a * c;
+    if (d === 0) {
+    let x1 = - b / (2 * a);
+    arr.push(x1);
+  } 
+  else if (d > 0) {
+    let x1 = (- b + Math.sqrt(d)) / (2 * a);
+    let x2 = (- b - Math.sqrt(d)) / (2 * a);
+    arr.push(x1,x2); 
+  }
+  return arr;
+  } 
 
-function calculateTotalMortgage(percent, contribution, amount, date) {
-  let totalAmount;
-
-  // код для задачи №2 писать здесь
-
-  return totalAmount;
-}
+  
